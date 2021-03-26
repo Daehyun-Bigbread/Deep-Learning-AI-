ceb37ee821c66f5e987678fb385edfbfa5ac6128
Daehyun Kim <bigdarkgold@gmail.com>
2021-03-26 10:50:06 +0900

    Add files via upload

---
+++ Deep_learning computer Vision Guide/data/util/install_tf113.sh
@@ -0,0 +1,14 @@
+pip install scikit-image
+pip install opencv-python
+pip install imgaug
+pip install lxml
+pip install pandas
+pip install selectivesearch
+pip install tensorflow-gpu==1.13.1
+pip install keras==2.2.1
+conda install -c conda-forge pycocotools -y
+conda install nb_conda -y
+
+# for  multi jupyter notebook kernal
+pip install ipykernel
+python -m ipykernel install --user --name tf113
---
+++ Deep_learning computer Vision Guide/data/util/install_tf115.sh
@@ -0,0 +1,14 @@
+pip install scikit-image
+pip install opencv-python
+pip install imgaug
+pip install lxml
+pip install pandas
+pip install selectivesearch
+pip install tensorflow-gpu==1.15.2
+pip install keras==2.3.0
+conda install -c conda-forge pycocotools -y
+conda install nb_conda -y
+
+# for multi jupyter kernel 
+pip install ipykernel
+python -m ipykernel install --user --name tf115
